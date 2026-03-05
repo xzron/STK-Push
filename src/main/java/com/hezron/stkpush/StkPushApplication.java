265580d0ef2719a4e1635c47fd6fca82d1d15e0f
bettonhezron <hezronbet@gmail.com>
2026-03-05 22:23:58 +0300

    Add initial Spring Boot project setup
    This commit initializes a Spring Boot project with a Maven build system. It includes basic configurations such as the `pom.xml` setup, application properties, MySQL data source integration, and dependency management. Additionally, `.gitattributes` and `.gitignore` files are added for version control, alongside Maven wrapper scripts for build automation.

---
+++ src/main/java/com/hezron/stkpush/StkPushApplication.java
@@ -0,0 +1,13 @@
+package com.hezron.stkpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StkPushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StkPushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hezron/stkpush/StkPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hezron.stkpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StkPushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
